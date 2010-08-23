b7b3a7617761d2f4e0aef2bb31a9479cc991366f
ngbrown <nathan@nkbrown.us>
2010-08-22 19:25:20 -0500

    Added WPF version for better debugging

---
+++ Caliburn.Micro.Hello/Caliburn.Micro.Hello.WPF/ShellViewModel.cs
@@ -0,0 +1,30 @@
+namespace Caliburn.Micro.Hello.WPF
+{
+    using System.Windows;
+
+    public class ShellViewModel : PropertyChangedBase
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this.  
+        }
+    }
+}
